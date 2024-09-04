b9f3bf5519ab6c231e060912d976eaf515470f78
abrahem haj ali <boss.abrahem@hotmail.com>
2024-09-04 08:16:06 +0300

    finished the primitive types homework

---
+++ Primitive Types/script.js
@@ -0,0 +1,20 @@
+const name = "Ibrahem";
+
+const number1 = 10;
+const number2 = 20;
+
+const result = number1 + number2;
+console.log(result); // 30
+console.log(number1 + number2); // 30
+
+const isValid = true;
+
+let total;
+console.log(total); // undefined
+
+total = 100;
+
+console.log(total); // 100
+
+let width = null;
+console.log(width); // null
