c8b51501d0e8cec9ea24b10b92762d4807dcf11e
abrahem haj ali <boss.abrahem@hotmail.com>
2024-09-04 15:46:33 +0300

    finished the string concatenation

---
+++ String Concatenation/script.js
@@ -0,0 +1,25 @@
+const myName = "John";
+const age = 25;
+const sentence = "My name is " + myName + " and I am " + age + " years old.";
+console.log(sentence); // My name is John and I am 25 years old.
+
+const favoriteColor = "blue";
+const favoriteFood = "pizza";
+const favoriteMovie = "Kung Fu Panda";
+const favorites =
+  "My favorite color is " +
+  favoriteColor +
+  ", I love the movie " +
+  favoriteMovie +
+  ", and I enjoy eating " +
+  favoriteMovie +
+  ".";
+console.log(favorites); // My favorite color is blue, I love the movie Kung Fu Panda, and I enjoy eating Kung Fu Panda.
+
+const a = 10;
+const b = 20;
+const result = "The sum of " + a + " and " + b + " is " + (a + b) + ".";
+console.log(result); // The sum of 10 and 20 is 30.
+
+const currentDay = "Monday";
+console.log(`Today is ${currentDay}`); // Today is Monday.
