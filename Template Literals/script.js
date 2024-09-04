21d6c3d94fc38f6502d22f23f6deb9833e932cc1
abrahem haj ali <boss.abrahem@hotmail.com>
2024-09-04 08:33:30 +0300

    finished the template literal homework

---
+++ Template Literals/script.js
@@ -0,0 +1,28 @@
+const firstName = "John";
+const age = 30;
+
+console.log(`My name is ${firstName} and I am ${age} years old.`); // My name is John and I am 30 years old.
+
+const length = 10;
+const width = 20;
+const area = length * width;
+console.log(
+  `The area of a recatangle with length of ${length} and width of ${width} is ${area}.`
+); // The area of a recatangle with length 10 and width 20 is 200.
+
+const today = "Monday";
+const task1 = "Go to school";
+const task2 = "Do homework";
+const task3 = "Play football";
+console.log(`Today is ${today}. I have to ${task1}, ${task2} and ${task3}.`); // Today is Monday. I have to Go to school, Do homework and Play football.
+
+const numberOfPets = 3;
+const typeOfPets = "cats";
+console.log(`I have ${numberOfPets} ${typeOfPets}.`); // I have 3 cats.
+
+const base = 3;
+const height = 5;
+const areaOfTriangle = (base * height) / 2;
+console.log(
+  `The area of a triangle with base of ${base} and height of ${height} is ${areaOfTriangle}.`
+); // The area of a triangle with base of 3 and height of 5 is 7.5.
