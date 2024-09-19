5d2c97d528bfd10427e4fb479d13f628e8614da5
abrahem haj ali <boss.abrahem@hotmail.com>
2024-09-19 14:42:17 +0300

    added the ability to insert the names of the players

--- weekend-exercies/CLI_Based_Golf_Card_Game.js
+++ weekend-exercies/CLI_Based_Golf_Card_Game.js
@@ -1,1 +1,26 @@
 const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const players = [
+  {
+    name: "",
+    cards: [],
+    isShown: [false, false, false, false],
+  },
+  {
+    name: "",
+    cards: [],
+    isShown: [false, false, false, false],
+  },
+];
+
+rl.question(`First player's name: `, (name) => {
+  players[0].name = name;
+  rl.question("Second player's name: ", (name) => {
+    players[1].name = name;
+  });
+});
