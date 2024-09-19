29c35d70cdc47785e1c6ea71595f436a44fbac23
abrahem haj ali <boss.abrahem@hotmail.com>
2024-09-19 11:15:27 +0300

    finished the mandatory questions

---
+++ Travel Planner/This and new/this_and_new.js
@@ -0,0 +1,105 @@
+function TravelPlanner() {
+  this.destinations = [];
+  this.flights = [];
+  this.accommodations = [];
+
+  this.addDestination = function (destination) {
+    this.destinations.push(destination);
+  };
+  this.addFlight = function (flight) {
+    this.flights.push(flight);
+  };
+  this.addAccommodation = function (accommodation) {
+    this.accommodations.push(accommodation);
+  };
+  this.calculateTotalCost = function () {
+    const totalCost = this.destinations.reduce(
+      (total, destination) => total + destination.calculateCost(),
+      0
+    );
+    const flightCost = this.flights.reduce(
+      (total, flight) => total + flight.calculateCost(),
+      0
+    );
+    const accommodationCost = this.accommodations.reduce(
+      (total, accommodation) => total + accommodation.calculateCost(),
+      0
+    );
+    return totalCost + flightCost + accommodationCost;
+  };
+  this.listDestinations = function () {
+    return this.destinations.map((destination) => destination.getInfo());
+  };
+  this.calculateTotalRevenue = function () {
+    return this.destinations.reduce(
+      (total, destination) => total + destination.revenue,
+      0
+    );
+  };
+  this.getDestinationByName = function (name) {
+    return this.destinations.find((destination) => destination.name === name);
+  };
+  this.Destination = function (country, city, baseCost) {
+    this.country = country;
+    this.city = city;
+    this.baseCost = baseCost;
+    this.getInfo = function () {
+      return `${this.city}, ${this.country}`;
+    };
+    this.calculateCost = function () {
+      return this.baseCost;
+    };
+    this.updateBaseCost = function (newCost) {
+      this.baseCost = newCost;
+    };
+  };
+  this.Flight = function (departure, arrivalCities, distance, pricePerMile) {
+    this.departure = departure;
+    this.arrivalCities = arrivalCities;
+    this.distance = distance;
+    this.pricePerMile = pricePerMile;
+    this.calculateCost = function () {
+      return this.distance * this.pricePerMile;
+    };
+    this.updatePricePerMile = function (newPrice) {
+      this.pricePerMile = newPrice;
+    };
+  };
+  this.Accommodation = function (HotelName, CostPerNight, numberOfNights) {
+    this.HotelName = HotelName;
+    this.CostPerNight = CostPerNight;
+    this.numberOfNights = numberOfNights;
+    this.calculateCost = function () {
+      return this.CostPerNight * this.numberOfNights;
+    };
+    this.applySeasonalDiscount = function (discount) {
+      this.CostPerNight *= discount;
+    };
+  };
+}
+
+const travelPlanner = new TravelPlanner();
+const destination1 = new travelPlanner.Destination("USA", "New York", 500);
+const destination2 = new travelPlanner.Destination("USA", "Los Angeles", 700);
+const flight1 = new travelPlanner.Flight(
+  "New York",
+  ["Los Angeles"],
+  2500,
+  0.1
+);
+const flight2 = new travelPlanner.Flight(
+  "Los Angeles",
+  ["New York"],
+  2500,
+  0.1
+);
+const accommodation1 = new travelPlanner.Accommodation("Hotel1", 100, 5);
+const accommodation2 = new travelPlanner.Accommodation("Hotel2", 200, 5);
+travelPlanner.addDestination(destination1);
+travelPlanner.addDestination(destination2);
+travelPlanner.addFlight(flight1);
+travelPlanner.addFlight(flight2);
+travelPlanner.addAccommodation(accommodation1);
+travelPlanner.addAccommodation(accommodation2);
+console.log(travelPlanner.calculateTotalCost());
+console.log(travelPlanner.listDestinations());
