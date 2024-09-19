6f60f5a050a1d62f6c6c2eee2fc51cb39d31f31c
abrahem haj ali <boss.abrahem@hotmail.com>
2024-09-19 14:43:38 +0300

    added the suits and cards array

--- weekend-exercies/CLI_Based_Golf_Card_Game.js
+++ weekend-exercies/CLI_Based_Golf_Card_Game.js
@@ -24,3 +24,21 @@ rl.question(`First player's name: `, (name) => {
     players[1].name = name;
   });
 });
+
+const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+
+const cards = [
+  { rank: "Ace", value: 1 },
+  { rank: "2", value: 2 },
+  { rank: "3", value: 3 },
+  { rank: "4", value: 4 },
+  { rank: "5", value: 5 },
+  { rank: "6", value: 6 },
+  { rank: "7", value: 0 },
+  { rank: "8", value: 8 },
+  { rank: "9", value: 9 },
+  { rank: "10", value: 10 },
+  { rank: "Jack", value: -1 },
+  { rank: "Queen", value: 12 },
+  { rank: "King", value: 13 },
+];
