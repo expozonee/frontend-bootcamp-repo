e3b54e69308b4d5186ed93d502820c5bdc232b81
abrahem haj ali <boss.abrahem@hotmail.com>
2024-09-19 17:27:34 +0300

    added the logic to take rounds

--- weekend-exercies/CLI_Based_Golf_Card_Game.js
+++ weekend-exercies/CLI_Based_Golf_Card_Game.js
@@ -1,10 +1,5 @@
 const readline = require("readline");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 const players = [
   {
     name: "",
@@ -17,13 +12,6 @@ const players = [
     isShown: [false, false, false, false],
   },
 ];
-
-// rl.question(`First player's name: `, (name) => {
-//   players[0].name = name;
-//   rl.question("Second player's name: ", (name) => {
-//     players[1].name = name;
-//   });
-// });
 
 const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
 
@@ -54,7 +42,6 @@ function createDeck() {
 }
 
 const deck = createDeck();
-console.log(deck);
 
 function shuffleDeck(deck) {
   for (let i = 0; i < deck.length; i++) {
@@ -66,4 +53,113 @@ function shuffleDeck(deck) {
 }
 
 shuffleDeck(deck);
-console.log(deck);
+let discardPile = [];
+
+function startGame() {
+  players.forEach((player) => {
+    for (let i = 0; i < 4; i++) {
+      player.cards.push(deck.pop());
+    }
+  });
+  discardPile.push(deck.pop());
+  taketurn(0);
+}
+
+function board() {
+  console.log("------ Board ------");
+
+  for (const player of players) {
+    let msg = [];
+    player.cards.forEach((card, index) => {
+      if (player.isShown[index]) {
+        msg.push(`[${card.rank} of ${card.suit}]`);
+      } else {
+        msg.push("[Face Down]");
+      }
+    });
+    console.log(`Hand of ${player.name}: `, msg.join(", "));
+  }
+  console.log(
+    `Discard Pile Top Card: ${
+      discardPile.length > 0
+        ? `${discardPile[discardPile.length - 1].rank} of ${
+            discardPile.length > 0 && discardPile[discardPile.length - 1].suit
+          }`
+        : "Empty"
+    } `
+  );
+  console.log("--------------------");
+}
+
+function takeAction(playerNumber) {
+  rl.question(
+    "Take an action: 1) Draw from Deck 2) Take from dicard pile: ",
+    (answer) => {
+      if (answer !== "1" && answer !== "2") takeAction();
+      if (answer === "1") {
+        const drawnCard = deck.pop();
+        console.log(`Card drawn is ${drawnCard.rank} of ${drawnCard.suit}`);
+        rl.question(
+          "Take an action: 1) Replace it with own card 2) Throw to discard pile: ",
+          (answer) => {
+            if (answer !== "1" && answer !== "2") {
+              rl.question(
+                "Which card you want to replace (1-4): ",
+                (cardNumber) => {
+                  players[playerNumber].cards[cardNumber - 1] = drawnCard;
+                  players[playerNumber].isShown[cardNumber - 1] = true;
+                  taketurn(playerNumber === 0 ? 1 : 0);
+                }
+              );
+            }
+            if (answer === "1") {
+              rl.question(
+                "Which card you want to replace (1-4): ",
+                (cardNumber) => {
+                  players[playerNumber].cards[cardNumber - 1] = drawnCard;
+                  players[playerNumber].isShown[cardNumber - 1] = true;
+                  taketurn(playerNumber === 0 ? 1 : 0);
+                }
+              );
+            }
+            if (answer === "2") {
+              discardPile.push(drawnCard);
+              taketurn(playerNumber === 0 ? 1 : 0);
+            }
+          }
+        );
+      }
+      if (answer === "2") {
+        const drawnCard = discardPile.pop();
+        rl.question("Which card you want to replace (1-4): ", (cardNumber) => {
+          players[playerNumber].cards[cardNumber - 1] = drawnCard;
+          players[playerNumber].isShown[cardNumber - 1] = true;
+          taketurn(playerNumber === 0 ? 1 : 0);
+        });
+      }
+    }
+  );
+}
+
+function taketurn(playerNumber) {
+  board();
+  console.log(`${players[playerNumber].name}'s turn!`);
+  takeAction(playerNumber);
+}
+
+function askForNames() {
+  rl.question(`First player's name: `, (name) => {
+    players[0].name = name;
+    rl.question("Second player's name: ", (name) => {
+      players[1].name = name;
+      startGame();
+    });
+  });
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+askForNames();
