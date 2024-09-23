c5f31dcf971990eeb26b3bd1f5353cae91a7927b
abrahem haj ali <boss.abrahem@hotmail.com>
2024-09-23 12:50:16 +0300

    finished question num 2

--- Travel Planner/Classes/OOP_Es6_Classes.js
+++ Travel Planner/Classes/OOP_Es6_Classes.js
@@ -96,4 +96,136 @@ class Trip {
 }
 
 const hello = new Trip("New York", 5, 100);
-console.log(hello.displayDetails());
+// console.log(hello.displayDetails());
+
+// 2
+
+class AdventureTrip extends Trip {
+  #diffcultyLevel;
+  #gearList;
+
+  constructor(destination, duration, distance, diffcultyLevel, gearList) {
+    super(destination, duration, distance);
+    this.#diffcultyLevel = diffcultyLevel;
+    this.#gearList = gearList;
+  }
+
+  get diffcultyLevel() {
+    return this.#diffcultyLevel;
+  }
+
+  get gearList() {
+    return this.#gearList;
+  }
+
+  set diffcultyLevel(level) {
+    const acceptedValues = ["easy", "moderate", "hard"];
+    if (!acceptedValues.includes(level)) {
+      console.error(
+        `Diffculty level must be one of these values only: ${acceptedValues.join(
+          ", "
+        )}`
+      );
+      return;
+    }
+    this.#diffcultyLevel = level;
+  }
+
+  set gearList(list) {
+    if (!Array.isArray(list)) {
+      console.error("The list must be an array.");
+      return;
+    }
+
+    if (list.every((item) => typeof item === "string")) {
+      console.error("It must be an array of strings");
+      return;
+    }
+
+    this.#gearList = list;
+  }
+
+  displayDetails = function () {
+    return `Destination Trip is: ${this.destination}\nDuration is: ${
+      this.duration
+    } days\nDistance is: ${this.distance} miles\ntotal cost is: $${
+      this.totalCost
+    }\nPlanned activites: ${
+      this.activites.length > 0
+        ? this.activites.join(", ")
+        : "No planned activites yet!"
+    }\nAdventure diffculty: ${
+      this.diffcultyLevel
+    }\nGear list: ${this.gearList.join(", ")}`;
+  };
+}
+
+const world = new AdventureTrip("New York", 5, 100, "easy", ["hat"]);
+// console.log(world.displayDetails());
+
+class CulturalTrip extends Trip {
+  #language;
+  #historicalSites;
+
+  constructor(destination, duration, distance, language, historicalSites) {
+    super(destination, duration, distance);
+    this.#language = language;
+    this.#historicalSites = historicalSites;
+  }
+
+  get language() {
+    return this.#language;
+  }
+
+  get historicalSites() {
+    return this.#historicalSites;
+  }
+
+  set language(lang) {
+    if (typeof lang !== "string") {
+      console.error("Language must be only text!");
+      return;
+    }
+
+    if (lang.length === 0) {
+      console.error("Language must not be empty!");
+      return;
+    }
+
+    this.#language = lang;
+  }
+
+  set historicalSites(sites) {
+    if (!Array.isArray(sites)) {
+      console.error("The list must be an array.");
+      return;
+    }
+
+    if (sites.every((site) => typeof site === "string")) {
+      console.error("It must be an array of strings");
+      return;
+    }
+
+    this.#historicalSites = sites;
+  }
+
+  displayDetails = function () {
+    return `Destination Trip is: ${this.destination}\nDuration is: ${
+      this.duration
+    } days\nDistance is: ${this.distance} miles\ntotal cost is: $${
+      this.totalCost
+    }\nPlanned activites: ${
+      this.activites.length > 0
+        ? this.activites.join(", ")
+        : "No planned activites yet!"
+    }\nLanguage: ${
+      this.language
+    }\nHistorical Sites: ${this.historicalSites.join(", ")}`;
+  };
+}
+
+const test = new CulturalTrip("Mountian", 10, 1000, "japanese", [
+  "mountain",
+  "lakes",
+]);
+console.log(test.displayDetails());
