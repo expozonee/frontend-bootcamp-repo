4d3bee1e5af38c2b6812c76b535c52a46aed5763
abrahem haj ali <boss.abrahem@hotmail.com>
2024-09-26 14:22:19 +0300

    finished the question

--- Event Listeners/Simple Click Counter/index.js
+++ Event Listeners/Simple Click Counter/index.js
@@ -1,8 +1,21 @@
 const button = document.querySelector("#clickButton");
 const counter = document.querySelector("#counter");
+const container = document.querySelector(".container");
+
+let currentCount;
 
 button.addEventListener("click", () => {
-  let currentCount = parseInt(counter.textContent);
-  currentCount += 1;
+  currentCount = parseInt(counter.textContent) + 1;
   counter.innerHTML = currentCount;
+  if (currentCount > 0) {
+    container.appendChild(newButton);
+  }
+});
+
+const newButton = document.createElement("button");
+newButton.innerHTML = "Reset";
+newButton.addEventListener("click", () => {
+  currentCount = 0;
+  counter.innerHTML = currentCount;
+  container.removeChild(newButton);
 });
