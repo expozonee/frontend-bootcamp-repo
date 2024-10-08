dd843490a0aa220d5e5e512f1ead3003145f3ece
abrahem haj ali <boss.abrahem@hotmail.com>
2024-10-09 01:41:26 +0300

    finished the logic for the rotation

--- Event Listeners/Character Carousel/script.js
+++ Event Listeners/Character Carousel/script.js
@@ -1,47 +1,51 @@
 const chars = document.querySelectorAll(".character");
 let pos = [0, 1, 2];
-let charData = [];
-changePos();
+let clickItem;
 
-function updatePos() {
-  //   debugger;
-  charData = [];
-  chars.forEach((char, index) => {
-    charData.push({
-      char,
-      pos: pos[index],
-    });
-  });
-  changePos();
+function initPos() {
+  updateCarousel();
 }
 
-updatePos();
+initPos();
 
 function updateCarousel() {
-  updatePos();
-
-  pos = pos.forEach((p, i) => {
-    if (i === pos.length - 1) {
-      pos[0] = p;
-    } else {
-      pos[i + 1] = p;
-    }
+  chars.forEach((c, index) => {
+    c.style.transform = `translateX(${
+      (pos.findIndex((p) => p === index) - 1) * 250
+    }px) ${pos.findIndex((p) => p === index) === 1 ? "scale(1.2)" : ""}`;
   });
 }
 
-chars.forEach((char) => {
-  char.addEventListener("click", () => {
-    // debugger;
+chars.forEach((char, index) => {
+  char.addEventListener("click", (e) => {
+    clickItem = e.target.parentElement;
+    const currentPos = pos.findIndex((p) => p === index);
+    if (currentPos === 1) return;
+
+    chars.forEach((c) => {
+      if (c.id === clickItem.id) {
+        c.classList.add("character-center");
+      } else {
+        c.classList.remove("character-center");
+      }
+    });
+
+    chars.forEach((c, index) => {
+      c.style.removeProperty("z-index");
+      if (c.id === clickItem.id) return;
+      if (currentPos > 1) {
+        c.style.zIndex = pos.findIndex((p) => p === index) === 1 ? 1 : 0;
+      } else {
+        c.style.zIndex = pos.findIndex((p) => p === index) === 1 ? 1 : 0;
+      }
+    });
+
+    if (currentPos > 1) {
+      pos.push(pos.shift());
+    } else {
+      pos.unshift(pos.pop());
+    }
+
     updateCarousel();
-    changePos();
   });
 });
-
-function changePos() {
-  charData.forEach((c) => {
-    const transform = c.char.style;
-    console.log(transform);
-
-    c.char.style.transform = `translateX(${(c.pos - 1) * 180}px)`;
-  });
-}
