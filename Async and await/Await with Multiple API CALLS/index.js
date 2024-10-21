0d86175dcca8b095dd2ea83114bfbc89afd2e002
abrahem haj ali <boss.abrahem@hotmail.com>
2024-10-21 20:22:08 +0300

    finished the first homework

---
+++ Async and await/Await with Multiple API CALLS/index.js
@@ -0,0 +1,53 @@
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+const usersContainer = document.getElementById("users");
+
+async function fetchUsers() {
+  const usersResponse = await fetch(`${BASE_URL}/users`);
+  const users = await usersResponse.json();
+
+  return users;
+}
+
+async function fetchPosts(id) {
+  if (typeof id !== "number" || id < 0) {
+    throw new Error("Id must be a valid number");
+  }
+
+  const postsResponse = await fetch(`${BASE_URL}/posts`);
+  const posts = await postsResponse.json();
+  const userPosts = posts.filter((post) => post.userId === id);
+
+  return userPosts;
+}
+
+async function displayUserAndPosts() {
+  try {
+    const users = await fetchUsers();
+
+    users.forEach(async (user) => {
+      const userContainerTemplate = document.getElementById("user-container");
+      const userContainer = userContainerTemplate.content.cloneNode(true);
+      const userNameElement = userContainer.querySelector(".user-name");
+      const userEmailElement = userContainer.querySelector(".user-email");
+      userNameElement.innerText = `Name: ${user.name}`;
+      userEmailElement.innerText = `Email: ${user.email}`;
+      try {
+        const userPosts = await fetchPosts(user.id);
+
+        userPosts.forEach((post) => {
+          const postTitle = userContainer.querySelector(".post-title");
+          const postBody = userContainer.querySelector(".post-body");
+          postTitle.innerHTML = `<strong>Title:</strong> ${post.title}`;
+          postBody.innerHTML = `<strong>Description:</strong>\n ${post.body}`;
+        });
+        usersContainer.appendChild(userContainer);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+displayUserAndPosts();
