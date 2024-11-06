3509dc9e3c534b7ba63a630538c0d8f0853ddcd8
abrahem haj ali <boss.abrahem@hotmail.com>
2024-11-06 11:36:39 +0200

    added Services, Tours, About and Footer components

--- Backroads-Application/src/App.jsx
+++ Backroads-Application/src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
+import About from "./components/About/About";
+import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
+import Services from "./components/Services/Services";
+import Tours from "./components/Tours/Tours";
 
 function App() {
   return (
     <>
       <NavBar />
       <Home />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
     </>
   );
 }
